86a19d352519d1af2b0fa9811f869d66fae355e8
Ascecent <andres85044@gmail.com>
2023-01-18 00:18:42 -0500

    Add Foreign Field Relation Between Users And Document Types

---
+++ database/migrations/2023_01_18_051011_add_document_type_field_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedTinyInteger('document_type_id');
+            $table->foreign('document_type_id')->references('id')->on('document_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['document_type_id']);
+            $table->dropColumn('document_type_id');
+        });
+    }
+};
