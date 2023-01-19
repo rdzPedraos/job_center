fe37cb07b72a491f995b46868cdd7901bd46d2b9
Ascecent <andres85044@gmail.com>
2023-01-19 00:19:26 -0500

    Add TemplateContractDetail Model
    Including migration and an empty seeder if needed

---
+++ app/Models/TemplateContractDetail.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TemplateContractDetail extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2023_01_19_051543_create_template_contract_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('template_contract_details', function (Blueprint $table) {
+            $table->comment('Contiene todos los detalles que de norma general tiene un tipo de contrato (tomando sólo la información como una plantilla)');
+
+            $table->unsignedInteger('id', true);
+            $table->text('description');
+            $table->unsignedSmallInteger('contract_type_id');
+            $table->enum('detail_type', ['R', 'F'])->comment('Almacena si el detalle es: R) Requisito para el cargo, F) Función del cargo');
+
+            $table->foreign('contract_type_id')->references('id')->on('contract_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('template_contract_details');
+    }
+};
