5a741056c231297332c3b46f4dfe73ef017954c2
Ascecent <andres85044@gmail.com>
2023-01-19 01:24:24 -0500

    Add City Model
    Including migration and an empty seeder if needed

---
+++ database/migrations/2023_01_19_062323_create_cities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->unsignedSmallInteger('id', true);
+            $table->unsignedSmallInteger('department_id');
+            $table->string('name', 50)->unique();
+            $table->string('zip_code', 5)->unique();
+
+            $table->foreign('department_id')->references('id')->on('departments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+};
