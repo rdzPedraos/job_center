474ffb2eb0db90350469346f9b334d7ff33c5b3a
Ascecent <andres85044@gmail.com>
2023-01-19 01:16:55 -0500

    Add RoadType Model
    Including migration and an empty seeder if needed

---
+++ database/migrations/2023_01_19_061529_create_road_types_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('road_types', function (Blueprint $table) {
+            $table->unsignedTinyInteger('id', true);
+            $table->string('name', 50)->unique();
+            $table->string('acronym', 5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('road_types');
+    }
+};
