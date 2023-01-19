dde9f769e46b50c189975ba917a68c12160a99e9
Ascecent <andres85044@gmail.com>
2023-01-19 01:37:20 -0500

    Add EducationLevel Model, Migration And Seeder

---
+++ database/migrations/2023_01_19_063359_create_education_levels_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('education_levels', function (Blueprint $table) {
+            $table->unsignedTinyInteger('id', true);
+            $table->string('name', 50)->unique();
+            $table->enum('type', ['PRE', 'POS'])->comment('PRE: Pregrado, POS: Posgrado');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('education_levels');
+    }
+};
