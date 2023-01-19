f390ee786b2c5e5c15825f9eb5f22b25761d8c65
Ascecent <andres85044@gmail.com>
2023-01-18 23:04:44 -0500

    Add AcademicFaculty Model
    Including migration and an empty seeder if needed

---
+++ database/migrations/2023_01_19_040347_create_academic_faculties_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('academic_faculties', function (Blueprint $table) {
+            $table->unsignedTinyInteger('id', true);
+            $table->string('name', 50)->unique();
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('academic_faculties');
+    }
+};
