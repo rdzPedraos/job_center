903cb279438f3e9f74e42b5a845ff7ffdc4aead3
Ascecent <andres85044@gmail.com>
2023-01-19 01:32:51 -0500

    Add Vaccine Model And Migration

---
+++ database/migrations/2023_01_19_063113_create_vaccines_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vaccines', function (Blueprint $table) {
+            $table->unsignedTinyInteger('id', true);
+            $table->string('name', 50)->unique();
+            $table->string('type', 40);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vaccines');
+    }
+};
