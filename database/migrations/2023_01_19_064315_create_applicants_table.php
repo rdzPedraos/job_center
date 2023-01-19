fa0818e44b84d9772e4e8733e5d09bdcaab709e6
Ascecent <andres85044@gmail.com>
2023-01-19 01:55:02 -0500

    Add Applicant Model, Migration And Seeder

---
+++ database/migrations/2023_01_19_064315_create_applicants_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applicants', function (Blueprint $table) {
+            $table->comment('Contiene todos los usuarios que ingresen con el fin de postularse a un cargo');
+
+            $table->unsignedBigInteger('id')->primary()->comment('Es el mismo código que tiene su usuario asociado.');
+            $table->unsignedSmallInteger('birth_place_id')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->unsignedSmallInteger('document_issue_city_id')->nullable();
+            $table->date('document_expedition_date')->nullable();
+            $table->unsignedMediumInteger('address_id')->nullable();
+            $table->unsignedTinyInteger('marital_status_id')->nullable();
+            $table->unsignedTinyInteger('blood_type_id')->nullable();
+            $table->enum('gender', ['M', 'F'])->nullable();
+            $table->unsignedTinyInteger('eps_id')->nullable();
+            $table->unsignedTinyInteger('pension_fund_id')->nullable();
+            $table->unsignedTinyInteger('arl_id')->nullable();
+            $table->string('military_passbook_number', 20)->nullable();
+            $table->string('family_contact_name', 100)->nullable();
+            $table->string('family_contact_phone', 15)->nullable();
+            $table->string('family_contact_relationship', 50)->nullable();
+            $table->timestamps();
+
+            $table->foreign('id')->references('id')->on('users');
+            $table->foreign('birth_place_id')->references('id')->on('cities');
+            $table->foreign('document_issue_city_id')->references('id')->on('cities');
+            $table->foreign('address_id')->references('id')->on('addresses');
+            $table->foreign('marital_status_id')->references('id')->on('marital_statuses');
+            $table->foreign('blood_type_id')->references('id')->on('blood_types');
+            $table->foreign('eps_id')->references('id')->on('eps');
+            $table->foreign('pension_fund_id')->references('id')->on('pension_funds');
+            $table->foreign('arl_id')->references('id')->on('arls');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applicants');
+    }
+};
