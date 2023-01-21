d9669f131d92062e87cb36dc50cc38a8a4b57b56
Ascecent <andres85044@gmail.com>
2023-01-21 16:03:06 -0500

    Add Relationships Methods To The Possible Parent Models of a DocumentHolder

--- app/Models/ApplicantExperience.php
+++ app/Models/ApplicantExperience.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class ApplicantExperience extends Model
 {
     use HasFactory;
+
+    /**
+     * Retrieve the documents uploaded as part of the experience of the applicant
+     */
+    public function documentHolders()
+    {
+        return $this->morphMany(DocumentHolder::class, 'documentable');
+    }
 }
--- app/Models/ApplicantStudy.php
+++ app/Models/ApplicantStudy.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class ApplicantStudy extends Model
 {
     use HasFactory;
+
+    /**
+     * Retrieve the documents uploaded as part of the studies of the applicant
+     */
+    public function documentHolders()
+    {
+        return $this->morphMany(DocumentHolder::class, 'documentable');
+    }
 }
