0c6400fec6e44da301f6562ad6178df1b59e7736
Ascecent <andres85044@gmail.com>
2023-01-22 11:48:31 -0500

    Add Relationship Methods To The Models

--- app/Models/JobRequest.php
+++ app/Models/JobRequest.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class JobRequest extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    /**
+     * Retrieve the job offer associated with the request
+     */
+    public function job()
+    {
+        return $this->belongsTo(JobOffer::class);
+    }
+
+    /**
+     * Retrieve the applicant that has made the request
+     */
+    public function applicant()
+    {
+        return $this->belongsTo(Applicant::class);
+    }
 }
--- app/Models/JobRequestStatus.php
+++ app/Models/JobRequestStatus.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class JobRequestStatus extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    /**
+     * Retrieve the requests associated with the status
+     */
+    public function requests()
+    {
+        return $this->hasMany(JobRequest::class);
+    }
 }
